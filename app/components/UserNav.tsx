a9a4be77f32cb18c2d8bc2b2234bb6dda45745b4
Santos Alejandro Arellano Olarte <arellanosantoso6@gmail.com>
2024-03-06 17:48:40 -0600

    Setting up Auth

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -8,30 +8,46 @@ import { MenuIcon } from "lucide-react";
 import {
     RegisterLink,
      LoginLink,
+     LogoutLink,
     } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 
-export function UserNav() {
-
+export async function UserNav() {
+    const {getUser} = getKindeServerSession()
+    const user = await getUser()
 return(
     <DropdownMenu>
         <DropdownMenuTrigger>
             <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
                 <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5"/>
                 <img 
-                src="https://www.uic.mx/posgrados/files/2018/05/default-user.png" 
+                  src= {
+                    user?.picture ?? "https://www.uic.mx/posgrados/files/2018/05/default-user.png"
+                   }
                 alt="Image of the user"
                 className="rounded-full h-8 w-8 hidden lg:block" />
             </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-[200px]">
+            { user ? (
+                  <>
+                  <DropdownMenuItem>
+                      <LogoutLink className="w-full">Logout</LogoutLink>
+                  </DropdownMenuItem>
+                  </>
+
+            ): (
+                <>
                 <DropdownMenuItem>
                     <RegisterLink className="w-full">Register</RegisterLink>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
                     <LoginLink className="w-full">Login</LoginLink>
                 </DropdownMenuItem>
-                
+                </>
+            )}
+
         </DropdownMenuContent>
     </DropdownMenu>
 )
