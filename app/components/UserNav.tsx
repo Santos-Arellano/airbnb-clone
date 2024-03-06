db091188406e9d0a154e48a744814e1a1a5e5fd7
Santos Alejandro Arellano Olarte <arellanosantoso6@gmail.com>
2024-03-06 16:31:50 -0600

    first commit

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,39 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+import {
+    RegisterLink,
+     LoginLink,
+    } from "@kinde-oss/kinde-auth-nextjs/components";
+
+
+export function UserNav() {
+
+return(
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+                <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5"/>
+                <img 
+                src="https://www.uic.mx/posgrados/files/2018/05/default-user.png" 
+                alt="Image of the user"
+                className="rounded-full h-8 w-8 hidden lg:block" />
+            </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuItem>
+                    <RegisterLink className="w-full">Register</RegisterLink>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <LoginLink className="w-full">Login</LoginLink>
+                </DropdownMenuItem>
+
+        </DropdownMenuContent>
+    </DropdownMenu>
+)
+
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+  <Button>Hello from te index page</Button>
+</div>    
+  );
+}
